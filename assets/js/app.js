3b40d2b63f71cb75bdae73da07fd38a9c51ba9c5
marianajane <mariana.musm@gmail.com>
2017-05-09 13:16:41 -0500

    Funcionalidadeliminar y mostrar pandas

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,26 @@
+var panda1 = document.getElementById("img1");
+var panda2 = document.getElementById("img2");
+var panda3 = document.getElementById("img3");
+var panda4 = document.getElementById("img4");
 
+var botonCerrar = document.getElementsByClassName("box-Im");
+console.log(botonCerrar);
+var array = Array.prototype.forEach.call(botonCerrar, function (elemento){ 
+	// this.style.display = "none"
+	elemento.addEventListener("click",eliminaPanda);
+})
 
-function cerrar() {
-    modal.style.display = "none";
+// botonCerrar[0].addEventListener("click", cerrarIm);
+
+function eliminaPanda(){
+	// alert("mata panadas x.x");
+	this.style.display="none";
 }
+
+function mostrarPandas() {
+	panda1.style.display="flex";
+	panda2.style.display="flex";
+	panda3.style.display="flex";
+	panda4.style.display="flex";
+}
+
