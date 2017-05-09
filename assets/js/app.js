e6e14ee9a64c1c6cac24f427338978d05de25719
marianajane <mariana.musm@gmail.com>
2017-05-09 15:52:32 -0500

    Funconalidad 80%

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,9 +2,10 @@ var panda1 = document.getElementById("img1");
 var panda2 = document.getElementById("img2");
 var panda3 = document.getElementById("img3");
 var panda4 = document.getElementById("img4");
+var par1 = document.getElementById("p1");
+var par2 = document.getElementById("p2");
 
 var botonCerrar = document.getElementsByClassName("box-Im");
-console.log(botonCerrar);
 var array = Array.prototype.forEach.call(botonCerrar, function (elemento){ 
 	// this.style.display = "none"
 	elemento.addEventListener("click",eliminaPanda);
@@ -24,3 +25,18 @@ function mostrarPandas() {
 	panda4.style.display="flex";
 }
 
+//Botones intermedios y párrafos
+
+var botonOrigen=document.getElementById("origenTxt")
+botonOrigen.addEventListener("click", origenMosOc);
+
+function origenMosOc() { 
+	par1.style.display = "none";
+}
+
+var botonExtincion=document.getElementById("extincionTxt")
+botonExtincion.addEventListener("click", extincionMosOc);
+
+function extincionMosOc() { 
+	par2.style.display = "none";
+}
